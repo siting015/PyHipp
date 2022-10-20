b0ced6cfb14f612baa04e973cb625a74e3ff547a
AWS ParallelCluster user <ec2-user@ip-10-0-0-252.ap-southeast-1.compute.internal>
2022-10-20 18:21:33 +0000

    updated pipe2a

--- pipe2a.sh
+++ pipe2a.sh
@@ -5,12 +5,6 @@
 # calls aligning_objects and raycast
 sbatch /data/src/PyHipp/rplparallel-slurm.sh
 
-# second set of jobs called from the day directory
-sbatch /data/src/PyHipp/rs1-slurm.sh
-sbatch /data/src/PyHipp/rs2-slurm.sh
-sbatch /data/src/PyHipp/rs3-slurm.sh
-sbatch /data/src/PyHipp/rs4-slurm.sh
-
 # second job - no dependencies, called from the day directory
 jid2=$(sbatch /data/src/PyHipp/rse-slurm.sh)
 
